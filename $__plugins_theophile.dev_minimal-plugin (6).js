ae5982f99888b87eeb74900720d435a44668c960
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2026-09-07 05:00:31 +0200

    Add files via upload

---
+++ $__plugins_theophile.dev_minimal-plugin (6).js
@@ -0,0 +1,4 @@
+{
+const tw = globalThis.$tw ??= Object.create(null);
+tw.preloadTiddlers = (tw.preloadTiddlers ?? []).concat([{"text":"{\"tiddlers\":{\"$:/plugins/theophile.dev/minimal-plugin/readme\":{\"text\":\"A minimal plugin\"},\"$:/plugins/theophile.dev/minimal-plugin/some tiddler\":{\"text\":\"A tiddler\"}}}","core-version":"5.4.1","type":"application/json","author":"theophile.dev","list":"readme","version":"0.0.1","description":"a minimal plugin","plugin-type":"plugin","stability":"STABILITY_1_EXPERIMENTAL","title":"$:/plugins/theophile.dev/minimal-plugin"}]);
+}
